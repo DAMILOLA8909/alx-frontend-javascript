3013e4a2a9a7b5501d2950cc5b2e8b372946685a
DAMILOLA8909 <damsony.soji@gmail.com>
2025-11-06 15:40:25 +0100

    debugging main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -100,8 +100,7 @@ interface StudentClassInterface {
  */
 
 function printTeacher(firstName: string, lastName: string): string {
-  const firstLetter = firstName.charAt(0);
-  return `${firstLetter}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
 // ==================== CLASS IMPLEMENTATION ====================
 
